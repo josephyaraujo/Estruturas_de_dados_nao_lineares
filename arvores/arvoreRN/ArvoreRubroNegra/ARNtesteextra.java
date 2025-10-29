d1a8ebdfc14d54d304b4fed0c1f7a84b40d3c62f
Josephy Cruz Araújo <josephy.araujo@escolar.ifrn.edu.br>
2025-10-29 22:39:52 +0000

    teste-extra

---
+++ arvores/arvoreRN/ArvoreRubroNegra/ARNtesteextra.java
@@ -0,0 +1,58 @@
+package arvores.arvoreRN.ArvoreRubroNegra;
+
+public class ARNtesteextra {
+     public static void main(String[] args){
+
+        System.out.println("----------Teste 1----------");
+        
+        ArvoreRN rn = new ArvoreRN(50);
+        rn.printArvore();
+        System.out.println("--------------------------");
+        System.out.println();
+        rn.inserirRN(30);
+        rn.printArvore();
+        System.out.println("--------------------------");
+        System.out.println();   
+        rn.inserirRN(70);
+        rn.printArvore();
+        System.out.println("--------------------------");
+        System.out.println();
+        rn.inserirRN(20);
+        rn.printArvore();
+        System.out.println("--------------------------");
+        System.out.println();
+        rn.inserirRN(40);
+        rn.printArvore();
+        System.out.println("--------------------------");
+        System.out.println();
+        rn.inserirRN(60);
+        rn.printArvore();
+        System.out.println("--------------------------");
+        System.out.println();
+        rn.inserirRN(80);
+        rn.printArvore();
+        System.out.println("--------------------------");
+        System.out.println();
+        rn.inserirRN(25);
+        rn.printArvore();
+        System.out.println("--------------------------");
+        System.out.println();
+        rn.inserirRN(35);
+        rn.printArvore();
+        System.out.println("--------------------------");
+        System.out.println();
+        rn.inserirRN(45);
+        rn.printArvore();
+        System.out.println("--------------------------");
+        System.out.println();
+            
+        //rn.inserirRN(25);
+        //rn.printArvore();
+        //System.out.println("--------------------------");
+        //System.out.println();
+
+        //rn.removerRN(5);     
+        //rn.printArvore();
+        //System.out.println();  
+     } 
+}
